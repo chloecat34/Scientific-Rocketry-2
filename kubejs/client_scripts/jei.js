62f170358da5b72b43dbb1aee044a82e7d642ad7
mcsaunders02 <mcsaunders02@wm.edu>
2025-02-04 23:59:03 -0500

    Removed tool leveling + updated jei.js

--- kubejs/client_scripts/jei.js
+++ kubejs/client_scripts/jei.js
@@ -8,4 +8,23 @@ JEIEvents.hideItems((event) => {
     event.hide("thermal:ruby_gear");
     event.hide("thermal:sapphire_gear");
 	event.hide("tconstruct:crafting_station");
+
+    	// Vanilla tools (besides pickaxes, because of Breaker Modules)
+	for (const toolType of ["shovel", "axe", "sword", "hoe"]) {
+		for (const toolMat of ["iron", "golden", "diamond", "wooden", "stone"]) {
+			event.hide(`minecraft:${toolMat}_${toolType}`);
+		}
+	}
+
+	// AE2 tools
+	for (const toolType of ["pickaxe", "shovel", "axe", "sword", "hoe"]) {
+		for (const toolMat of ["nether_quartz", "certus_quartz", "fluix"]) {
+			event.hide(`ae2:${toolMat}_${toolType}`);
+		}
+	}
+	
+	// IE tools
+	for (const toolType of ["pickaxe", "shovel", "axe", "hoe", "sword"]) {
+		event.hide(`immersiveengineering:${toolType}_steel`);
+	}
 });
