209e8c096105287c03d42e5d461a9af27c3677a4
chloecat34 <milesman034@gmail.com>
2025-07-30 12:42:26 -0400

    Removed modlist from cache

--- modlist.py
+++
@@ -1,8 +0,0 @@
-# This creates a list of the mods in the pack. I made this since I don't have a manifest set up yet.
-import os
-
-
-with open("modlist.txt", "w+") as f:
-    for name in os.listdir("mods"):
-        if name != ".index":
-            f.write(f"{name}\n")
