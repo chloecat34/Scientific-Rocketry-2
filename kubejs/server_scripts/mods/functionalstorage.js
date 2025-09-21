e0a8b8444d694b309d4491cdef6a7081a1f682d5
chloecat34 <milesman034@gmail.com>
2025-09-21 03:11:49 -0400

    Functional storage upgrade textures

--- kubejs/server_scripts/mods/functionalstorage.js
+++ kubejs/server_scripts/mods/functionalstorage.js
@@ -52,4 +52,57 @@ ServerEvents.recipes((event) => {
         event.replaceInput({ output: drawer }, "minecraft:bucket", "pneumaticcraft:small_tank");
         event.replaceInput({ output: drawer }, "#minecraft:planks", "pneumaticcraft:reinforced_bricks");
     });
+
+    // Upgrades
+    event.remove({ output: "functionalstorage:copper_upgrade" });
+    event.remove({ output: "functionalstorage:gold_upgrade" });
+    event.remove({ output: "functionalstorage:diamond_upgrade" });
+    event.remove({ output: "functionalstorage:netherite_upgrade" });
+
+    event.shaped("functionalstorage:copper_upgrade", ["ABA", "CDC", "ABA"], {
+        A: "#forge:plates/brass",
+        B: "#forge:storage_blocks/brass",
+        C: "create:item_vault",
+        D: "#functionalstorage:drawer",
+    });
+
+    event.shaped("functionalstorage:gold_upgrade", ["ABA", "CDC", "BAB"], {
+        A: "#forge:plates/aluminum",
+        B: "#forge:storage_blocks/aluminum",
+        C: "immersiveengineering:component_electronic_adv",
+        D: "functionalstorage:copper_upgrade",
+    });
+
+    event.custom({
+        type: "pneumaticcraft:pressure_chamber",
+        inputs: [
+            {
+                item: "functionalstorage:gold_upgrade",
+            },
+            {
+                item: "pneumaticcraft:printed_circuit_board",
+            },
+            {
+                type: "pneumaticcraft:stacked_item",
+                item: "actuallyadditions:emeradic_crystal",
+                count: 4,
+            },
+            {
+                type: "pneumaticcraft:stacked_item",
+                tag: "forge:plates/vibrant_alloy",
+                count: 8,
+            },
+            {
+                type: "pneumaticcraft:stacked_item",
+                item: "actuallyadditions:advanced_coil",
+                count: 2,
+            },
+        ],
+        pressure: 4.5,
+        results: [
+            {
+                item: "functionalstorage:diamond_upgrade",
+            },
+        ],
+    });
 });
