ba8fbc43015bceee60cf13e711fe6da46395ba39
chloecat34 <chloe.saunders02@icloud.com>
2025-12-05 16:43:16 -0500

    Cosmic tools recipes

--- kubejs/server_scripts/multiblocks/lava_enricher.js
+++ kubejs/server_scripts/multiblocks/lava_enricher.js
@@ -5,4 +5,12 @@ ServerEvents.recipes((event) => {
         .inputFluids("minecraft:lava 250", "tconstruct:ichor 250")
         .outputFluids("tconstruct:blazing_blood 250")
         .duration(100);
+
+    // Lava enricher controller
+    event.shaped("gtceu:lava_enricher", ["ABA", "CDC", "ABA"], {
+        A: "tconstruct:scorched_bricks",
+        B: "tconstruct:scorched_soul_glass",
+        C: "create:precision_mechanism",
+        D: "#forge:gears/cobalt",
+    });
 });
