e04e33c0135aae1ed5232b71139c99ee194f4a11
chloecat34 <chloe.saunders02@icloud.com>
2026-01-03 16:37:14 -0500

    Stainless steel tweak

--- kubejs/server_scripts/multiblocks/fluid_drilling_rig.js
+++ kubejs/server_scripts/multiblocks/fluid_drilling_rig.js
@@ -28,7 +28,7 @@ ServerEvents.recipes((event) => {
         .notConsumable("immersiveengineering:fluid_pump")
         .outputFluids("kubejs:salt_water 500")
         .dimension("minecraft:overworld")
-        .biome("minecraft:is_ocean")
+        .biome("ocean")
         .duration(10)
         .EUt(128);
 
